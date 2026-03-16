6524e3230f74e6ca2361ee62d1247d94a5468fc1
PikbeeOnCode <pikbeekrish@gmail.com>
2026-03-16 13:06:07 +0545

    feat: add toggleCommentLike and toggletweetLike functionalities with respective routes

--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -6,6 +6,7 @@ import { User } from "../models/user.models.js";
 import { Video } from "../models/video.models.js";
 import { Comment } from "../models/comment.models.js";
 import { Like } from "../models/like.models.js";
+import { Tweet } from "../models/tweet.models.js";
 
 
 const toggleVideoLike = asyncHandler(async(req,res)=>{
@@ -45,8 +46,83 @@ const toggleVideoLike = asyncHandler(async(req,res)=>{
 })
 
 
+const toggleCommentLike = asyncHandler(async(req,res)=>{
+    const commentId = req.params.commentId;
+    const user = req.user
+
+    if(!commentId){
+        throw new apiError(400,"Comment id is required");
+    }
+
+    const comment = await Comment.findById(commentId);
+
+    if(!comment){
+        throw new apiError(404,"Comment not found");
+    }
+
+    const existingLike = await Like.findOne({
+        comment: commentId,
+        likedBy: user._id,
+    })
+
+    if(existingLike){
+      const dislike =   await Like.findByIdAndDelete(existingLike._id);
+        return res.status(200).json(new apiResponse(200,dislike,"Comment unliked successfully"));
+    }
+
+    if(!existingLike){
+        const newLike = await Like.create({
+            comment: commentId,
+            likedBy: user._id,
+        })
+        if(!newLike){
+            throw new apiError(500,"Failed to like the comment");
+        }
+        return res.status(201).json(new apiResponse(201,newLike,"Comment liked successfully"));
+    }
+})
+
+const toggletweetLike = asyncHandler(async(req,res)=>{
+    const tweetId = req.params.tweetId;
+    const user = req.user
+
+    if(!tweetId){
+        throw new apiError(400,"Tweet id is required");
+    }
+
+    const tweet = await Tweet.findById(tweetId);
+
+    if(!tweet){
+        throw new apiError(404,"Tweet not found");
+    }
+
+    const existingLike = await Like.findOne({
+        tweet: tweetId,
+        likedBy: user._id,
+    })
+
+    if(existingLike){
+      const dislike =   await Like.findByIdAndDelete(existingLike._id);
+        return res.status(200).json(new apiResponse(200,dislike,"Tweet unliked successfully"));
+    }
+
+    if(!existingLike){
+        const newLike = await Like.create({
+            tweet: tweetId,
+            likedBy: user._id,
+        })
+        if(!newLike){
+            throw new apiError(500,"Failed to like the tweet");
+        }
+        return res.status(201).json(new apiResponse(201,newLike,"Tweet liked successfully"));
+    }
+})
+
+
 
 
 export {
-     toggleVideoLike 
+     toggleVideoLike ,
+     toggleCommentLike,
+     toggletweetLike
     }
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,11 +1,16 @@
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import { Router } from "express";
-
 import { 
     toggleVideoLike,
+    toggleCommentLike,
+    toggletweetLike
  } from "../controllers/like.controllers.js";
+
+
 const router = Router();
 
 router.route("/togglevideolike/:videoId").post(verifyJwt, toggleVideoLike);
+router.route("/togglecommentlike/:commentId").post(verifyJwt, toggleCommentLike);
+router.route("/toggletweetlike/:tweetId").post(verifyJwt, toggletweetLike);
 
 export default router;
