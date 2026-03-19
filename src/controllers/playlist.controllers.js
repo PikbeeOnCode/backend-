5ce0a2bb688c96c7bda18cf17f897ad8bd7a97d0
PikbeeOnCode <pikbeekrish@gmail.com>
2026-03-19 11:21:53 +0545

    feat: implement addVideoToPlaylist and getUserPlaylists functionalities with respective routes

--- src/controllers/playlist.controllers.js
+++ src/controllers/playlist.controllers.js
@@ -1,4 +1,3 @@
-import { Video } from "../models/video.models.js";
 import { Playlist } from "../models/playlist.models.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { apiError } from "../utils/apiError.js";
@@ -30,10 +29,125 @@ const createPlaylist = asyncHandler(async(req,res)=>{
 
 });
 
+const addVideoToPlaylist = asyncHandler(async(req,res)=>{
+    const user = req.user;
+    const {PlaylistId,videoid}= req.params;
 
+    
+    if(!PlaylistId){
+        throw new apiError(400, "Playlist id is required");
+    }
+
+    if(!mongoose.Types.ObjectId.isValid(PlaylistId)){
+        throw new apiError(400, "Invalid playlist id");
+    }
+
+      if(!videoid){
+        throw new apiError(400, "Video id is required");
+    }
+
+    if(!mongoose.Types.ObjectId.isValid(videoid)){
+        throw new apiError(400, "Invalid video id");
+    }
+
+
+
+    const playlist = await Playlist.findById(PlaylistId);
+
+    if(!playlist){
+        throw new apiError(404, "Playlist not found");
+    }
+
+    if(playlist.owner.toString() !== user._id.toString()){
+        throw new apiError(403, "You are not authorized to add video to this playlist");
+    }
+
+        const wasAlreadyInPlaylist = playlist.videos.some(
+        vid => vid.toString() === videoid
+    );
+
+    if (wasAlreadyInPlaylist) {
+        return res.status(200).json(
+            new apiResponse(
+                200,
+                playlist,               
+                "This video is already in the playlist"
+            )
+        );
+    }
+  
+
+    const addedvideoplaylist = await Playlist.findByIdAndUpdate(PlaylistId,{
+        $addToSet:{videos:videoid}
+    },{
+        new:true
+    })
+
+    if(!addedvideoplaylist){
+        throw new apiError(404, "Playlist not found");
+    }
+
+    return res.
+    status(200).
+    json(new apiResponse(200,  addedvideoplaylist,"Video added to playlist successfully"))
+    
+})
+
+const getUserPlaylists = asyncHandler(async(req,res)=>{
+    const userId = req.params.userId;
+
+    if(!userId){
+        throw new apiError(400, "User id is required");
+    }
+
+    if(!mongoose.Types.ObjectId.isValid(userId)){
+        throw new apiError(400, "Invalid user id");
+    }
+    
+
+    const playlists = await Playlist.find(
+        {owner:userId}
+    )
+
+    return res.
+    status(200).
+    json(new apiResponse(200, playlists,"User playlists fetched successfully"))
+})
+
+const getPlaylistbyId = asyncHandler(async(req,res)=>{
+    const playlistId = req.params.playlistId;
+
+    if(!playlistId){
+        throw new apiError(400, "Playlist id is required");
+    }
+
+    if(!mongoose.Types.ObjectId.isValid(playlistId)){
+        throw new apiError(400, "Invalid playlist id");
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+        .populate({
+            path: "videos",
+            select: "title description thumbnail owner views duration isPublished createdAt"
+        })
+        .populate({
+            path: "owner",
+            select: "username fullname avatar"
+        });
+
+    if(!playlist){
+        throw new apiError(404, "Playlist not found");
+    }
+
+    return res
+        .status(200)
+        .json(new apiResponse(200, playlist, "Playlist fetched successfully"));
+})
 
 export {
     createPlaylist,
-
+    addVideoToPlaylist,
+    getUserPlaylists,
+    getPlaylistbyId
 }
    
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -2,9 +2,15 @@ import { verifyJwt } from "../middlewares/auth.middleware.js";
 import { Router } from "express";
 import {
     createPlaylist,
+    addVideoToPlaylist,
+    getUserPlaylists,
+    getPlaylistbyId
 } from "../controllers/playlist.controllers.js";
 const router = Router();
 
 router.route("/createplaylist").post(verifyJwt, createPlaylist);
+router.route("/addvideo/:PlaylistId/:videoid").post(verifyJwt, addVideoToPlaylist);
+router.route("/getplaylists/:userId").get(verifyJwt, getUserPlaylists);
+router.route("/getplaylist/:playlistId").get(verifyJwt, getPlaylistbyId);
 
 export default router;
